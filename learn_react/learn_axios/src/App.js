55d6177df69b0d5d1f8ecdf03dbd043a08dd8297
Oliver-zg <982205369@qq.com>
2020-12-17 16:37:56 +0800

    learn_axios

--- learn_react/learn_axios/src/App.js
+++ learn_react/learn_axios/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+  //发送网络请求
+  async componentDidMount() {
+    // try {
+    //   const result = await axios({
+    //     url: 'https://httpbin.org/get',
+    //     params: {
+    //       name: 'zg',
+    //       age: 18
+    //     }
+    //   })
+    //   console.log(result);
+    // } catch (error) {
+    //   //捕获异常
+    // }
+
+    //axios.all 的使用
+    // const request1 = axios({
+    //   url: 'https://httpbin.org/get',
+    //   params: {
+    //     name: 'zg',
+    //     age: 18
+    //   }
+    // })
+    // const request2 = axios({
+    //   url: 'https://httpbin.org/post',
+    //   data: {
+    //     name: 'Oliver',
+    //     age:19
+    //   },
+    //   method: 'post'
+    // })
+    
+    // axios.all([request1,request2]).then(res => {
+    //   console.log(res);
+    // })
+
+    //创建实例
+    // const instance = axios.create({
+    //   baseURL: '',
+    //   timeout: 5000
+    // })
+    // axios({
+
+    // })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- learn_react/learn_redux/src/index.js
+++ learn_react/learn_redux/src/index.js
@@ -1,10 +1,8 @@
-import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-//默认配置
-axios.defaults.baseURL = 'https://httpbin.org'
-axios.defaults.timeout = 5000
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,3 +14,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
